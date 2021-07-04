5c25bf5a90426a8e4f15b80d5bfb4d97eba07ae1
Роман Политов <RIPolitov@sberbank.ru>
2021-07-04 17:52:05 +0300

    add Longest Subarray with Ones after Replacement solution

--- Sliding_Windows/Longest Subarray with Ones after Replacement.py
+++ Sliding_Windows/Longest Subarray with Ones after Replacement.py
@@ -3,4 +3,23 @@
 
 
 def length_of_longest_substring(arr: [], k: int) -> int:
-    return -1
+    left, result, zeros_count = 0, 0, 0
+
+    for right in range(len(arr)):
+        right_char = arr[right]
+        if right_char == 0:
+            zeros_count += 1
+
+        if zeros_count > k:
+            left_dig = arr[left]
+            if left_dig == 0:
+                zeros_count -= 1
+            left += 1
+
+        result = max(result, right - left + 1)
+
+    return result
+
+
+print(length_of_longest_substring([0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], k=2))
+print(length_of_longest_substring([0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], k=3))
